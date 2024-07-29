457cbcd0681cb1cce647b3e911d428e4ccd546c0
Blake Gall <bg849619@ohio.edu>
2024-07-28 20:34:36 -0400

    Changed kustomization to always pull. Added logging for default redis url.

--- bot/src/database/index.js
+++ bot/src/database/index.js
@@ -7,5 +7,8 @@ client.on('error', (error) => {
 });
 
 export function start() {
+    if(!process.env.REDIS_URL) {
+        console.warn("No REDIS_URL environment variable found. Defaulting to redis://localhost:6379.");
+    }
     client.connect(process.env.REDIS_URL || 'redis://localhost:6379');
 }
