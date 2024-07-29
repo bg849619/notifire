c80786e4a1ead87ac7a3cf0db5be46bac81eb7c1
Blake Gall <bg849619@ohio.edu>
2024-07-28 20:46:05 -0400

    Now crash if REDIS_URL is not found.

--- bot/src/database/index.js
+++ bot/src/database/index.js
@@ -8,7 +8,8 @@ client.on('error', (error) => {
 
 export function start() {
     if(!process.env.REDIS_URL) {
-        console.warn("No REDIS_URL environment variable found. Defaulting to redis://localhost:6379.");
+        console.warn("No REDIS_URL environment variable found.");
+        process.exit(1);
     }
-    client.connect(process.env.REDIS_URL || 'redis://localhost:6379');
+    client.connect(process.env.REDIS_URL);
 }
